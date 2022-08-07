c93dcd95094fdb34d34c58aa28beeb75b08881e7
VonBorstel-J <jordanvonborstel@gmail.com>
2022-08-07 13:47:08 -0700

    Added cookies for HW2

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const bodyParser = require('body-parser');
 const expressSession = require('express-session') ({ 
     secret: 'secret',
     resave: false, 
-    saveUninitialized: false
+    saveUninitialize: true, 
+    cookie: { 
+        secure: false,
+        maxAge: 90000
+    }
 }); 
 
 app.use(bodyParser.json()); 
